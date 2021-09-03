129e1c218283a015693e1c9aba17e5faa5888f26
a.bertanzon <a.bertanzon@ferlini.it>
2021-09-03 20:49:46 +0200

    Added UpButton to the Activity. This commit adds the code to add the Up Button to the activity_main.xml

--- app/src/main/java/com/abcode/shoestore/MainActivity.kt
+++ app/src/main/java/com/abcode/shoestore/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.abcode.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.abcode.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
